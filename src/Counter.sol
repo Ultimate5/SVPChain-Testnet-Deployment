9e272a72fa7dea0113953ca0797264894bed650e
Ultimate5 <122797566+Ultimate5@users.noreply.github.com>
2026-09-19 08:53:52 +0000

    commit

---
+++ src/Counter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Counter {
+        uint256 public count;
+            event Incremented(uint256 newCount);
+                function increment() external {
+                            count += 1;
+                                    emit Incremented(count);
+                }
+                    function setCount(uint256 _count) external {
+                                count = _count;
+                    }
+}
+                }
+}
