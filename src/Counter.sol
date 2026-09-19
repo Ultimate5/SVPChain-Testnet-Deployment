e4bf88d72f95345c9ec8f7c8fc902c8506ffb4f5
Ultimate5 <122797566+Ultimate5@users.noreply.github.com>
2026-09-19 09:50:01 +0000

    commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.19;
 
 contract Counter {
-        uint256 public count;
-            event Incremented(uint256 newCount);
-                function increment() external {
-                            count += 1;
-                                    emit Incremented(count);
-                }
-                    function setCount(uint256 _count) external {
-                                count = _count;
-                    }
-}
-                }
+    uint256 public count;
+
+    event Incremented(uint256 newCount);
+
+    function increment() external {
+        count += 1;
+        emit Incremented(count);
+    }
+
+    function setCount(uint256 _count) external {
+        count = _count;
+    }
 }
